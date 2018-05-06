602ad13608243ee97ba6ff0e9868a357bf92a2ed
panjunye <dev@panjunye.com>
2018-05-06 23:14:12 +0800

    Init commit

---
+++ src/main/java/io/peony/playtodo/TodoTaskApplication.java
@@ -0,0 +1,12 @@
+package io.peony.playtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTaskApplication.class, args);
+	}
+}
